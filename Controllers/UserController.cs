ec98e09ad014b8b970a2ae7d1b03cec05f4f4fd4
dangv <dangv@DESKTOP-1Q9MS11>
2023-10-17 11:57:09 +0700

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLThuVien.Models;
+
+namespace QLThuVien.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        public ActionResult TraCuuSach()
+        {
+            return View();
+        }
+    }
+}
